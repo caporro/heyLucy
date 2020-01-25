e5559ddbf7eced49f4712d45fd46b7dfecee7971
Claudio Caporro <claudio@caporro.it>
2020-01-25 14:59:49 +0100

    del keys

--- keys.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $bot_token = '368702863:AAG8e36YUdJejCihRo45wTw_q0t0DgkmdqQ';
-    $webhook_url = 'https://caporro.it/lucybot/webhook.php';
-
-//CryptoLucy 368702863:AAG8e36YUdJejCihRo45wTw_q0t0DgkmdqQ
-//HeyLucy 368702863:AAG8e36YUdJejCihRo45wTw_q0t0DgkmdqQ
